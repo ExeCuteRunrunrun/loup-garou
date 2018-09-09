a488cf430666c49908b7d4d00d4c1f26fa08d673
Manying Zhang <manying.zhang.execute@gmail.com>
2018-09-09 17:05:17 +0200

    update

--- intent_classification_w2v.py
+++ intent_classification_w2v.py
@@ -7,6 +7,7 @@ from keras.models import Sequential,load_model
 from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam, SGD
 
 import numpy as np
 import pandas as pd
@@ -20,7 +21,7 @@ df = pd.read_csv('data_speech_all.csv', sep='\t', encoding='utf-8')
 X  = df['segmented']
 y  = df['intent']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=20)
 
 ### Encode labels
 encoder     = LabelBinarizer()
@@ -30,15 +31,15 @@ y_test_enc  = encoder.transform(y_test)
 
 ### Create sequence
 vocabulary_size = 5000
-batch_size      = 50
+batch_size      = 100
 tokenizer       = Tokenizer(num_words= vocabulary_size, char_level = False)
 tokenizer.fit_on_texts(X_train)
 word_index      = tokenizer.word_index
 
 sequences       = tokenizer.texts_to_sequences(X_train) #mode='tfidf')
-X_train_enc     = pad_sequences(sequences, maxlen=30)
+X_train_enc     = pad_sequences(sequences, maxlen=10)
 seque_test      = tokenizer.texts_to_sequences(X_test) #mode='tfidf')
-X_test_enc      = pad_sequences(seque_test, maxlen=30)
+X_test_enc      = pad_sequences(seque_test, maxlen=10)
 
 def modelquora(pretrained_path="sgns.zhihu.word"):
     dic_quora = {}
@@ -66,54 +67,55 @@ embedding_matrix = embeddingMarix(word_index, EMBEDDING_DIM=300)
 
 ## create model
 model_w2v = Sequential()
-model_w2v.add(Embedding(vocabulary_size, 300, input_length=30, weights=[embedding_matrix], trainable=False))
-model_w2v.add(Dropout(0.2))
-model_w2v.add(LSTM(100))
+model_w2v.add(Embedding(vocabulary_size, 300, input_length=10, weights=[embedding_matrix], trainable=True))
+model_w2v.add(Dropout(0.4))
+model_w2v.add(LSTM(100,recurrent_dropout=0.4))
 model_w2v.add(Dense(8, activation='sigmoid'))
-model_w2v.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+adam = Adam(lr=0.01)
+model_w2v.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
 # checkpointer = ModelCheckpoint(filepath="/w2v_b50_mlen30_d02_e50.hdf5", verbose=1, save_best_only=True)
 # callbackss = [checkpointer] # permission denied
 ## Fit train data
-model_w2v.fit(X_train_enc, y_train_enc,batch_size=batch_size, validation_split=0.1, epochs=3)#, callbacks=callbackss)
+model_w2v.fit(X_train_enc, y_train_enc,batch_size=batch_size, validation_split=0.1, epochs=20)#, callbacks=callbackss)
 
 ### Evaluate the model
 score = model_w2v.evaluate(X_test_enc, y_test_enc,
                        batch_size=batch_size, verbose=1)
 
-print('Test accuracy:', score[1])
+print('Test accuracy: False embedding 300, drop 0.4, lr 0.01, input_length',10, score[1])
 
-### Save Tokenizer i.e. Vocabulary
-with open('tokw2v_b50_mlen30_d02_e50.pickle', 'wb') as handle:
-    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-model_w2v.model.save('w2v_b50_mlen30_d02_e50.h5')
-labels = encoder.classes_
-print(labels)
-
-### Load model and tokenizer
-model_w2v = load_model('w2v_b50_mlen30_d02_e50.h5')
-# model_w2v = Sequential()
-# model_w2v.add(Embedding(5000, 300, input_length=30))
-# model_w2v.add(LSTM(300, dropout=0.2, recurrent_dropout=0.2))
-# model_w2v.add(Dense(8, activation='sigmoid'))
-# model_w2v.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model_w2v.load_weights('w2v_b50_mlen30_d02_e50.h5')
-
-
-with open('tokw2v_b50_mlen30_d02_e50.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-
-x_data = "我 也 觉得 很 奇怪，你 这个 3号 为什么 要去 聊 5号 和 9号 呢。你 是个 民 的话 你 绝对 不会 找 女巫。在我看来 3号 狼面 很大，因为 他 在 这个 位置 去 找神，让我 觉得 他 不是 个 好身份。我 认 5号 7号 好身份，9号 跳 女巫，我 还是 比较 可以 接受 的吧。今晚 出 这个 3号".split('。')
-
-x_data_series = pd.Series(x_data)
-
-x_tokenized = tokenizer.texts_to_sequences(x_data_series)#, mode='tfidf')
-# print(x_tokenized)
-x_pad       = pad_sequences(x_tokenized, maxlen=30)
-
-
-for x in x_pad:
-    prediction = model_w2v.predict(np.array([x]))
-    predicted_label = labels[np.argmax(prediction[0])]
-    print("Predicted label: " + predicted_label, x)
+# ### Save Tokenizer i.e. Vocabulary
+# with open('tokw2v_b50_mlen30_d02_e50.pickle', 'wb') as handle:
+#     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# model_w2v.model.save('w2v_b50_mlen30_d02_e50.h5')
+# labels = encoder.classes_
+# print(labels)
+#
+# ### Load model and tokenizer
+# model_w2v = load_model('w2v_b50_mlen30_d02_e50.h5')
+# # model_w2v = Sequential()
+# # model_w2v.add(Embedding(5000, 300, input_length=30))
+# # model_w2v.add(LSTM(300, dropout=0.2, recurrent_dropout=0.2))
+# # model_w2v.add(Dense(8, activation='sigmoid'))
+# # model_w2v.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # model_w2v.load_weights('w2v_b50_mlen30_d02_e50.h5')
+#
+#
+# with open('tokw2v_b50_mlen30_d02_e50.pickle', 'rb') as handle:
+#     tokenizer = pickle.load(handle)
+#
+# x_data = "我 也 觉得 很 奇怪，你 这个 3号 为什么 要去 聊 5号 和 9号 呢。你 是个 民 的话 你 绝对 不会 找 女巫。在我看来 3号 狼面 很大，因为 他 在 这个 位置 去 找神，让我 觉得 他 不是 个 好身份。我 认 5号 7号 好身份，9号 跳 女巫，我 还是 比较 可以 接受 的吧。今晚 出 这个 3号".split('。')
+#
+# x_data_series = pd.Series(x_data)
+#
+# x_tokenized = tokenizer.texts_to_sequences(x_data_series)#, mode='tfidf')
+# # print(x_tokenized)
+# x_pad       = pad_sequences(x_tokenized, maxlen=30)
+#
+#
+# for x in x_pad:
+#     prediction = model_w2v.predict(np.array([x]))
+#     predicted_label = labels[np.argmax(prediction[0])]
+#     print("Predicted label: " + predicted_label, x)
